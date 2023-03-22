01111fa1a4c8b3d985eb7c8d20b33d418a7ed5ad
gbrielchaves9 <gbrielchaves9@gmai.com>
2023-03-21 21:25:53 -0300

    layout tipo 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import React, { createContext, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Primeira from "./pages/Primeira";
+import Segunda from "./pages/Segunda";
 
 
+export const UserContext = createContext(null);
 
 export default function App() {
-
+  const [user, setUser] = useState(null);
   return (
-   
+    <UserContext.Provider value={{ user, setUser }}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Primeira />} />
-        
-         
+          <Route path="/cadastro" element={<Segunda />} />
         </Routes>
       </BrowserRouter>
+    </UserContext.Provider>
   );
 }
+
--- src/pages/Primeira.js
+++ src/pages/Primeira.js
@@ -1,19 +1,64 @@
-import React from 'react';
-import styled from "styled-components"
+import React, { useState } from 'react';
+import axios from 'axios';
 import logo from "../assets/logo.png";
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { UserContext } from "../App";
+import { useNavigate } from "react-router-dom";
 
 
 export default function Primeira() {
+    const { setUser } = useContext(UserContext);
+    const [email, setEmail] = useState("")
+    const [	password, setSenha] = useState("")
+    const navigate = useNavigate();
 
+    function handleLogin(user) {
+        setUser(user);
+        navigate("/Habitos");
+    }
+
+    function enviaDados(e) {
+        e.preventDefault();
+        const dadosConta = { email,password };
+        axios
+            .post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", dadosConta)
+            .then((res) => {
+                handleLogin(res.data);
+                alert("login ok")
+            })
+            .catch((err) => {
+                alert("erro ")
+                console.log(err.data);
+                console.log(err.dadosConta);
+            });
+    }
 
     return (
         <PageContainer>
-            <SessionContainer data-test="movie-day">
-                <img src={logo} alt={"carregando"} />
-            </SessionContainer>
+            <img src={logo} alt={"carregando"} />
 
 
 
+            <ListContainer  >
+                <form onSubmit={enviaDados}>
+                    <input type="email" placeholder="E-mail"
+                        required
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input type="password" placeholder="senha"
+                        required
+                        value={	password}
+                        onChange={e => setSenha(e.target.value)}
+                    />
+                    <button data-test="book-seat-btn" type="submit" >Entrar</button>
+                </form>
+            </ListContainer>
+            <Link to={`/cadastro`} data-test="movie" >
+                <p>Não tem uma conta? Cadastre-se!</p>
+            </Link>
         </PageContainer>
 
 
@@ -21,21 +66,42 @@ export default function Primeira() {
 }
 
 const PageContainer = styled.div`
-   height: 667px;
-width: 100%;
-background: #0E0E13;
-margin-top:0px;
-margin-bottom:0px;
-
-
-`
-const SessionContainer = styled.div`
-   
+background-color:black;
     display: flex;
+    flex-direction: column;
     align-items: center;
-    top :0;
-    img {
-        height: 50px;
-width: 299px;
-        }
+    font-size: 24px;
+    text-align: center;
+    color: #293845;
+    margin-top: 65px;
+    img{
+        height: 178px;
+        width: 180px;
+    }
+    p{
+        font-family: Lexend Deca;
+font-size: 14px;
+font-weight: 400;
+text-align: center;
+    }
+`
+const ListContainer = styled.label`
+background-color:black;
+    width: calc(100vw - 40px); 
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 20px 0;
+    font-size: 18px;
+    button {
+        align-self: center;
+        background: #52B6FF;
+    }
+    input {
+        width: calc(100vw - 60px);
+        height: 45px;
+left: 36px;
+top: 279px;
+border-radius: 5px;
+    }
 `
